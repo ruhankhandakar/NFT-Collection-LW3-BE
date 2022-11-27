4d71f967ccf6018393ef295c6fd0db339cfc5651
Ruhan Khandakar <khandakarruhan@gmail.com>
2022-11-27 20:59:25 +0530

    update metadata url

--- constant.ts
+++ constant.ts
@@ -1,3 +1,3 @@
 export const WHITELIST_CONTRACT_ADDRESS =
   '0xDaE2A33De179cA42F71BE10D87f6310de99136D3';
-export const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+export const METADATA_URL = 'https://ruhan-nft-collection.vercel.app/api/';
